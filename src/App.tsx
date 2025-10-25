35e4af68d5120cb04d589fe1cc4d425be4c175ca
NikiRenegade <nikirenegade@gmail.com>
2025-10-25 20:01:12 +0700

    Убрано лишнее из изначального шаблона

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
 import './App.css'
 import CatFacts from "./components/CatFacts.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <CatFacts count={count} setCount={setCount} />
+    <CatFacts/>
   )
 }
 
